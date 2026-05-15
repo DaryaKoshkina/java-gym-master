20a7fea8b45150b0605a9a57370d0ff26277e128
Darya <daria.koshkina228@yandex.ru>
2026-05-15 16:32:14 +0500

    feat: реализовано расписание тренировок и тесты

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,17 +4,48 @@ import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
-
+    private final Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable  = new HashMap<>();
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        //сохраняем занятие в расписании
+        DayOfWeek day = trainingSession.getDayOfWeek();
+        TimeOfDay time = trainingSession.getTimeOfDay();
+
+        // 1. Если дня еще нет, создаем для него TreeMap
+        if (!timetable.containsKey(day)) {
+            timetable.put(day, new TreeMap<>());
+        }
+
+        TreeMap<TimeOfDay, List<TrainingSession>> dayMap = timetable.get(day);
+
+        // 2. Если на это время еще нет тренировок, создаем новый список ArrayList
+        if (!dayMap.containsKey(time)) {
+            dayMap.put(time, new ArrayList<>());
+        }
+
+        // 3. Добавляем тренировку в список для этого времени
+        dayMap.get(time).add(trainingSession);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public Map<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        TreeMap<TimeOfDay, List<TrainingSession>> dayMap = timetable.get(dayOfWeek);
+        if (dayMap == null) {
+            return Map.of();
+        }
+        return dayMap;
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        TreeMap<TimeOfDay, List<TrainingSession>> dayMap = timetable.get(dayOfWeek);
+
+        if (dayMap == null) {
+            return List.of();
+        }
+
+        List<TrainingSession> sessions = dayMap.get(timeOfDay);
+
+        if (sessions == null) {
+            return List.of();
+        }
+
+        return sessions;
     }
 }
